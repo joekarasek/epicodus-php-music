1b79fc946d39301b64b91835b4cbc9695b4001a9
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-25 12:33:08 -0800

    wrote test for save function of Genre

--- tests/GenreTest.php
+++ tests/GenreTest.php
@@ -1,15 +1,31 @@
 <?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
     require_once __DIR__ . '/../src/Genre.php';
 
-    class ClassTest extends PHPUnit_Framework_TestCase
+    $server = 'mysql:host=localhost;dbname=music_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class GenreTest extends PHPUnit_Framework_TestCase
     {
-        function test_methodToTest_inputDescription()
+        function test_save()
         {
             // Arrange
+            $type = 'Classic Rock';
+            $test_Genre = new Genre($type);
 
             // Act
+            $test_Genre->save();
 
             // Assert
+            $result = Genre::getAll();
+            $this->assertEquals($test_Genre, $result[0]);
         }
     }
  ?>
