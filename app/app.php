63942eb39ecf48648b8dab4068202b6a8187c57b
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-25 12:17:37 -0800

    add setup to server to app.php

--- app/app.php
+++ app/app.php
@@ -1,10 +1,17 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
 
+    // create silex object with twig templating
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
+
+    // setup server for database
+    $server = 'mysql:host=localhost;dbname=music';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
